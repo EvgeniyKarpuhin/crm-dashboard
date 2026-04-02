05394be5742e9f95426274624af9e80528f7d3bc
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2026-04-02 14:49:05 +0300

    added structure for the Dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,28 @@
+import { useTaskStore } from "../store/useTaskStore";
+import { useClientStore } from "../store/useClientStore";
+
 const Dashboard = () => {
-    return <div>Dashboard page</div>;
+    const { tasks } = useTaskStore();
+    const { clients } = useClientStore();
+
+    const total = tasks.length;
+    const newCount = tasks.filter((t) => t.status === "new").length;
+    const inProgress = tasks.filter((t) => t.status === "in-progress").length;
+    const done = tasks.filter((t) => t.status === "done").length;
+
+    const completionRate = total
+        ? Math.round((done / total) * 100)
+        : 0;
+
+    return (
+        <div className="space-y-6">
+            <h2 className="text-2xl font-bold">Dashboard</h2>
+
+            <div className="grid grid-cols-4 gap-4">
+               
+            </div>
+        </div>
+    )
 };
 
 export default Dashboard;
